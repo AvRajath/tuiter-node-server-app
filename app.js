d226e62818b9e6ea62dd0638c1ed937e751cd676
AvRajath <rajathav28@gmail.com>
2022-12-03 02:26:10 -0500

    a9 Mongo fa22

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ import TuitsController from "./controllers/tuits/tuits-controller.js";
 
 import mongoose from "mongoose";
 const CONNECTION_STRING =
-  "mongodb+srv://rav28:ganapati@cluster0.hvrmafp.mongodb.net/?retryWrites=true&w=majority";
+  process.env.DB_CONNECTION_STRING || "mongodb://localhost:27017/tuiter";
 
 let x = mongoose.connect(CONNECTION_STRING);
 
